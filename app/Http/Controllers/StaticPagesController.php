f0351137a922a7932d3d6e881b3fe7edec418a7d
vagrant <vagrant@homestead>
2019-10-05 09:55:11 +0000

    访问策略

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,27 +6,36 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
+
+    public function home(){
+        return view('static_pages/home');
+    }
+     public function help(){
+        return view('static_pages/help');
+    }
+     public function about(){
+        return view('static_pages/about');
+    }
     //
+    //  public function create(){
+    //     return view('sessions.create');
+    // }
 
-     public function create(){
-        return view('sessions.create');
-    }
+    // public function store(Request $request){
+    //     $credentials = this->validate($reqeust,[
+    //             'email'=>'required|email|max:255',
+    //             'password'=>'required',
+    //         ]);
 
-    public function store(Request $request){
-        $credentials = this->validate($reqeust,[
-                'email'=>'required|email|max:255',
-                'password'=>'required',
-            ]);
-
-        if(Auth::attempt($credentials)){
-            session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
-        }else{
-            session()->flash('danger','抱歉，邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
-    }
-    public function destory(){
-        return;
-    }
+    //     if(Auth::attempt($credentials)){
+    //         session()->flash('success','欢迎回来！');
+    //         return redirect()->route('users.show',[Auth::user()]);
+    //     }else{
+    //         session()->flash('danger','抱歉，邮箱和密码不匹配');
+    //         return redirect()->back()->withInput();
+    //     }
+    // }
+    // public function destory(){
+    //     return;
+    // }
 }
