190122da89642aa1c5245eba5056b7423a6c5290
vagrant <vagrant@homestead>
2019-10-05 02:12:38 +0000

    创建会话

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,13 +8,25 @@ class StaticPagesController extends Controller
 {
     //
 
-    public function home(){
-    	return view('static_pages/home');
+     public function create(){
+        return view('sessions.create');
     }
-    public function help(){
-    	return view('static_pages/help');
+
+    public function store(Request $request){
+        $credentials = this->validate($reqeust,[
+                'email'=>'required|email|max:255',
+                'password'=>'required',
+            ]);
+
+        if(Auth::attempt($credentials)){
+            session()->flash('success','欢迎回来！');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            session()->flash('danger','抱歉，邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
     }
-    public function about(){
-    	return view('static_pages/about');
+    public function destory(){
+        return;
     }
 }
