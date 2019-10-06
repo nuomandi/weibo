eca4868cc40aab6bbe43ba1d468ab5975af58059
vagrant <vagrant@homestead>
2019-10-06 10:04:44 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct(){
 
         $this->middleware('auth',[
-            'except'=>['show','create','store']
+            'except'=>['show','create','store','index']
             ]);
 
          $this->middleware('guest', [
@@ -71,6 +71,13 @@ class UsersController extends Controller
              session()->flash('success','资料更新成功!');
              return redirect()->route('users.show',$user);
     }
+
+    public function index(){
+        $users=User::all();
+        return view('users.index',compact('users'));
+    }
+
+
 
 
 
